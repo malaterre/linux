96bc6cec6dfb6b5584b0153566fcc8f2a62cff13
Daniel Wagner <daniel.wagner@bmw-carit.de>
2016-02-10 10:21:21 +0100

    mips: Differentiate between 32 and 64 bit ELF header
    Depending on the configuration either the 32 or 64 bit version of
    elf_check_arch() is defined. parse_crash_elf{32|64}_headers() does
    some basic verification of the ELF header via
    vmcore_elf{32|64}_check_arch() which happen to map to elf_check_arch().
    Since the implementation 32 and 64 bit version of elf_check_arch()
    differ, we use the wrong type:
    
       In file included from include/linux/elf.h:4:0,
                        from fs/proc/vmcore.c:13:
       fs/proc/vmcore.c: In function 'parse_crash_elf64_headers':
    >> arch/mips/include/asm/elf.h:228:23: error: initialization from incompatible pointer type [-Werror=incompatible-pointer-types]
         struct elfhdr *__h = (hdr);     \
                              ^
       include/linux/crash_dump.h:41:37: note: in expansion of macro 'elf_check_arch'
        #define vmcore_elf64_check_arch(x) (elf_check_arch(x) || vmcore_elf_check_arch_cross(x))
                                            ^
       fs/proc/vmcore.c:1015:4: note: in expansion of macro 'vmcore_elf64_check_arch'
          !vmcore_elf64_check_arch(&ehdr) ||
           ^
    
    Therefore, we rather define vmcore_elf{32|64}_check_arch() as a
    basic machine check and use it also in binfm_elf?32.c as well.
    
    Signed-off-by: Daniel Wagner <daniel.wagner@bmw-carit.de>
    Suggested-by: Maciej W. Rozycki <macro@imgtec.com>
    Reviewed-by: Maciej W. Rozycki <macro@imgtec.com>
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/12529/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/elf.h
+++ arch/mips/include/asm/elf.h
@@ -196,7 +196,7 @@ typedef elf_fpreg_t elf_fpregset_t[ELF_NFPREG];
 	int __res = 1;							\
 	struct elfhdr *__h = (hdr);					\
 									\
-	if (__h->e_machine != EM_MIPS)					\
+	if (!mips_elf_check_machine(__h))				\
 		__res = 0;						\
 	if (__h->e_ident[EI_CLASS] != ELFCLASS32)			\
 		__res = 0;						\
@@ -227,7 +227,7 @@ typedef elf_fpreg_t elf_fpregset_t[ELF_NFPREG];
 	int __res = 1;							\
 	struct elfhdr *__h = (hdr);					\
 									\
-	if (__h->e_machine != EM_MIPS)					\
+	if (!mips_elf_check_machine(__h))				\
 		__res = 0;						\
 	if (__h->e_ident[EI_CLASS] != ELFCLASS64)			\
 		__res = 0;						\
@@ -253,6 +253,11 @@ typedef elf_fpreg_t elf_fpregset_t[ELF_NFPREG];
 #define ELF_ARCH	EM_MIPS
 
 #endif /* !defined(ELF_ARCH) */
+
+#define mips_elf_check_machine(x) ((x)->e_machine == EM_MIPS)
+
+#define vmcore_elf32_check_arch mips_elf_check_machine
+#define vmcore_elf64_check_arch mips_elf_check_machine
 
 struct mips_abi;
 
